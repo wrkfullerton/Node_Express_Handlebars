f0f324592f07778c3067229c29561e6414d7abf1
William Fullerton <wrk.fullerton@gmail.com>
2020-08-20 22:43:34 -0500

    added files for the homework

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Patty Mely');
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
+INSERT INTO burgers (burger_name, devoured) VALUES ('A1 Thick N Hearty', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet N Spicy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style', true);
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-condition").on("click", function(event) {
+      var id = $(this).data("id");
+      var newCondition = $(this).data("newcondition");
+  
+      var newFoodState = {
+        devoured: newCondition
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newFoodState
+      }).then(
+        function() {
+          console.log("changed condition to", newCondition);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
